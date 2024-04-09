ff00aa8ceaad03538a0098eb0ed2efe1540abd7a
MichaelOgunyankin <149902350+MichaelOgunyankin@users.noreply.github.com>
2024-04-09 11:52:51 +0100

    fetched recipe items image, id, image and publishe and displayed from API

--- recipe_app/src/context/index.jsx
+++ recipe_app/src/context/index.jsx
@@ -5,6 +5,8 @@ export const GlobalContext = createContext(null)
 export default function GlobalState({ children }) {
 
     const [searchParam, setSearchParam] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [recipeList, setRecipeList] = useState([])
 
     async function handleSubmit(event) {
         event.preventDefault()
@@ -13,13 +15,22 @@ export default function GlobalState({ children }) {
             const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`)
 
             const data = await res.json()
+            if (data?.data?.recipes) {
+                setRecipeList(data?.data?.recipes)
+                setLoading(false)
+                setSearchParam('')
+            }
 
             console.log(data);
 
         } catch (e) {
             console.log(e);
+            setLoading(false)
+            setSearchParam('')
         }
     }
 
-    return <GlobalContext.Provider value={{ searchParam, setSearchParam, handleSubmit }} >{children}</GlobalContext.Provider>
+    console.log(loading, recipeList);
+
+    return <GlobalContext.Provider value={{ searchParam, loading, recipeList,    setSearchParam, handleSubmit }} >{children}</GlobalContext.Provider>
 }
